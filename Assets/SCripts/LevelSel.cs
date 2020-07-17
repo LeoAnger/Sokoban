7cf0c9091d85f8f2ddb56833bc6e8f01f93a938b
Anger <17335301995@163.com>
2020-07-17 23:59:42 +0800

    添加选关场景...

---
+++ Assets/SCripts/LevelSel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void TestDug()
+    {
+        Debug.Log("触发事件...");
+    }
+    
+    // 开始游戏
+    public void StartGame()
+    {
+        //    1. 获取地图
+        //    2.跳转场景
+        Invoke("ChangeScene", 1);  // 1s后切换场景
+    }
+    
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("推箱子代码版");  // 登陆成功则切换到游戏界面
+    }
+}
